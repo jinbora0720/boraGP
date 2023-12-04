a6551602d85e518b25b70aed1938828f1130fa06
jinbora0720 <jinbora0720@gmail.com>
2023-12-04 20:15:46 +0100

    attached C_ in front of mkNNIndx, mkNNIndxCB, mkUIndx, PGLogit + version 0.1.8

--- src/PGLogit.cpp
+++ src/PGLogit.cpp
@@ -1,15 +1,20 @@
+#define USE_FC_LEN_T
 #include <string>
+#include "util.h"
+#include "rpg.h"
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 #include <R.h>
 #include <Rmath.h>
 #include <Rinternals.h>
 #include <R_ext/Linpack.h>
 #include <R_ext/Lapack.h>
 #include <R_ext/BLAS.h>
-#include "util.h"
-#include "rpg.h"
-
-#ifdef _OPENMP
-#include <omp.h>
+#ifndef FCONE
+# define FCONE
 #endif
 
 extern "C" {
@@ -22,11 +27,9 @@ extern "C" {
     const double negOne = -1.0;
     const double zero = 0.0;
     char const *lower = "L";
-    char const *upper = "U";
     char const *ntran = "N";
     char const *ytran = "T";
-    char const *rside = "R";
-    char const *lside = "L";
+    
     
     //get args
     double *y = REAL(y_r);
@@ -37,7 +40,7 @@ extern "C" {
     int nSamples = INTEGER(nSamples_r)[0];
     int nThreads = INTEGER(nThreads_r)[0];
     int verbose = INTEGER(verbose_r)[0];
- 
+    
 #ifdef _OPENMP
     omp_set_num_threads(nThreads);
 #else
@@ -67,7 +70,7 @@ extern "C" {
     double *omega = (double *) R_alloc(n, sizeof(double));
     double *kappa = (double *) R_alloc(n, sizeof(double));
     double *yStr = (double *) R_alloc(n, sizeof(double));
-
+    
     //return stuff  
     SEXP betaSamples_r;
     PROTECT(betaSamples_r = allocMatrix(REALSXP, p, nSamples)); nProtect++;
@@ -80,7 +83,7 @@ extern "C" {
     double *tmp_p2 = (double *) R_alloc(p, sizeof(double));
     double *tmp_n = (double *) R_alloc(n, sizeof(double));
     double *tmp_np = (double *) R_alloc(np, sizeof(double));
-	
+    
     for(i = 0; i < n; i++){
       kappa[i] = y[i] - static_cast<double>(nTrial[i])/2.0;
     }
@@ -88,48 +91,48 @@ extern "C" {
     GetRNGstate();
     
     for(s = 0; s < nSamples; s++){
-
+      
       //update augs
       for(i = 0; i < n; i++){
-	omega[i] = rpg(nTrial[i], F77_NAME(ddot)(&p, &X[i], &n, beta, &inc));
-	yStr[i] = kappa[i]/omega[i];
+        omega[i] = rpg(nTrial[i], F77_NAME(ddot)(&p, &X[i], &n, beta, &inc));
+        yStr[i] = kappa[i]/omega[i];
       }
-           
+      
       //update beta 
       for(i = 0; i < n; i++){
-	tmp_n[i] = yStr[i]*omega[i];
+        tmp_n[i] = yStr[i]*omega[i];
       }
-      F77_NAME(dgemv)(ytran, &n, &p, &one, X, &n, tmp_n, &inc, &zero, tmp_p, &inc); 	  
-
+      F77_NAME(dgemv)(ytran, &n, &p, &one, X, &n, tmp_n, &inc, &zero, tmp_p, &inc FCONE); 	  
+      
       for(i = 0; i < n; i++){
-	for(j = 0; j < p; j++){
-	  tmp_np[j*n+i] = X[j*n+i]*omega[i];
-	}
+        for(j = 0; j < p; j++){
+          tmp_np[j*n+i] = X[j*n+i]*omega[i];
+        }
       }
-
-      F77_NAME(dgemm)(ytran, ntran, &p, &p, &n, &one, X, &n, tmp_np, &n, &zero, tmp_pp, &p);
-
-      F77_NAME(dpotrf)(lower, &p, tmp_pp, &p, &info); if(info != 0){error("c++ error: dpotrf here failed\n");}
-      F77_NAME(dpotri)(lower, &p, tmp_pp, &p, &info); if(info != 0){error("c++ error: dpotri here failed\n");}
-      F77_NAME(dsymv)(lower, &p, &one, tmp_pp, &p, tmp_p, &inc, &zero, tmp_p2, &inc);
-      F77_NAME(dpotrf)(lower, &p, tmp_pp, &p, &info); if(info != 0){error("c++ error: dpotrf here failed\n");}
+      
+      F77_NAME(dgemm)(ytran, ntran, &p, &p, &n, &one, X, &n, tmp_np, &n, &zero, tmp_pp, &p FCONE FCONE);
+      
+      F77_NAME(dpotrf)(lower, &p, tmp_pp, &p, &info FCONE); if(info != 0){error("c++ error: dpotrf here failed\n");}
+      F77_NAME(dpotri)(lower, &p, tmp_pp, &p, &info FCONE); if(info != 0){error("c++ error: dpotri here failed\n");}
+      F77_NAME(dsymv)(lower, &p, &one, tmp_pp, &p, tmp_p, &inc, &zero, tmp_p2, &inc FCONE);
+      F77_NAME(dpotrf)(lower, &p, tmp_pp, &p, &info FCONE); if(info != 0){error("c++ error: dpotrf here failed\n");}
       mvrnorm(beta, tmp_p2, tmp_pp, p);
-
+      
       //save samples  
       F77_NAME(dcopy)(&p, beta, &inc, &REAL(betaSamples_r)[s*p], &inc);
-
+      
       R_CheckUserInterrupt();
     }
     
     PutRNGstate();
-
+    
     //make return object
     SEXP result_r, resultName_r;
     int nResultListObjs = 1;
-
+    
     PROTECT(result_r = allocVector(VECSXP, nResultListObjs)); nProtect++;
     PROTECT(resultName_r = allocVector(VECSXP, nResultListObjs)); nProtect++;
-
+    
     SET_VECTOR_ELT(result_r, 0, betaSamples_r);
     SET_VECTOR_ELT(resultName_r, 0, mkChar("p.beta.samples")); 
     
